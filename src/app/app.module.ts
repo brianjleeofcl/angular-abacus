8f1eb2ade0ed0b97d86974b62a771a9cf153c180
Brian Lee <brianjleeofcl@gmail.com>
2017-10-27 17:14:51 -0700

    Add abacus, rod components, add basic styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RodComponent } from './rod/rod.component';
+import { AbacusComponent } from './abacus/abacus.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RodComponent,
+    AbacusComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/rod/rod.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RodComponent } from './rod.component';
+
+describe('RodComponent', () => {
+  let component: RodComponent;
+  let fixture: ComponentFixture<RodComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RodComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RodComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
